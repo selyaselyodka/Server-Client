b1b4cbb4812d6e3947430b6cdb65471e0c08055a
KeanuChandra <143045678+KeanuChandra@users.noreply.github.com>
2024-11-13 16:14:48 -0800

    Add files via upload

--- rpi_client.py
+++ rpi_client.py
@@ -5,6 +5,9 @@ import csv
 from datetime import datetime
 import os.path
 
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
 
 HOST = '192.168.4.1'  # The server's hostname or IP address
 PORT = 65432     # The port used by the server
@@ -16,11 +19,55 @@ def process_data_from_server(x):
 
 def reciever():
     try:
-        while True:
+        start_time = time.time()
+        fig, ax = plt.subplots()
+        x_data, y_data1, y_data2, y_data3 = [], [], [], []
+        line1, = ax.plot([], [], 'b-', label="Temp 1")
+        line2, = ax.plot([], [], 'r-', label="Temp 2")
+        #line3, = ax.plot([], [], 'g-', label="Temp 3")
+
+        ax.set_title("Temperature Plot")
+        ax.legend()
+
+        def init():
+            ax.set_ylim(0, 100)
+            return line1, line2
+        
+        def update(frame):
             data = client_socket.recv(1024).decode('utf-8')
-            x_temperature,y_humidity = process_data_from_server(data)
-            data_dict = [{'temp':x_temperature,'humidity':y_humidity}]
-            writer.writerows(data_dict)
+            current_time = time.time() - start_time
+            #Temporary Measure
+            #data = data.replace(',', '')
+
+            values = data.split(',')
+            values.pop()
+
+            index = 1
+            data_dict = {}
+            for v in values:
+                key = 'temp '+str(index)
+                data_dict[key] = v
+                index += 1
+
+            
+            #x_temperature,y_humidity = process_data_from_server(data)
+            #data_dict = [{'temp':data}]
+            writer.writerows([data_dict])
+            x_data.append(current_time)
+            y_data1.append(float(data_dict['temp 1']))
+            y_data2.append(float(data_dict['temp 2']))
+            #y_data3.append(float(data_dict['temp 3']))
+            line1.set_data(x_data, y_data1)
+            line2.set_data(x_data, y_data2)
+            #line3.set_data(x_data, y_data3)
+            ax.set_xlim(max(0, x_data[-1] - 10), x_data[-1] + 1)
+            #ax.set_ylim(min(min(y_data1), min(y_data2), min(y_data3)) - 1, max(max(y_data1), max(y_data2), max(y_data3)) + 1) 
+            ax.set_ylim(min(min(y_data1), min(y_data2)) - 1, max(max(y_data1), max(y_data2)) + 1)    
+            return line1, line2
+        ani = FuncAnimation(fig, update, init_func=init, interval=1000, blit=False)
+        plt.show()    
+        
+
 
     finally:
          client_socket.close()
@@ -40,7 +87,7 @@ if __name__ == "__main__":
 
     file_path = THIS_FOLDER+"/data/"+dt+".csv"
     csvfile = open(file_path, 'w+')
-    fieldnames = ["temp","humidity"]
+    fieldnames = ["temp 1","temp 2","temp 3"]
     writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
     writer.writeheader()
 
